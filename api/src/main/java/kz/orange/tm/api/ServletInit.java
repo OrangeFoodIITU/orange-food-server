e687db7dc2a80d4b08a58da1b0ae0c3ca1a0c389
miramas <fimafiz@gmail.com>
2018-09-24 13:42:39 +0600

    api module

---
+++ api/src/main/java/kz/orange/tm/api/ServletInit.java
@@ -0,0 +1,11 @@
+package kz.orange.tm.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInit extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(ApiApplication.class);
+    }
+}
